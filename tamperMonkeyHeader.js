a86e8db66031922288bfdd9b0362e16e8dc617d7
fosemberg <fosemberg@gmail.com>
2020-02-01 10:22:15 +0300

    add tamperMonkeyHeader

---
+++ tamperMonkeyHeader.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name         accent words
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        *://*/*
+// @grant        none
+// ==/UserScript==
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 var path = require('path');
+var webpack = require("webpack");
+var fs = require("fs");
 
 module.exports = {
     mode: 'development',
@@ -6,5 +8,11 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'index.bundle.js'
-    }
+    },
+    plugins: [
+        new webpack.BannerPlugin({
+            banner: fs.readFileSync('./tamperMonkeyHeader.js', 'utf8'),
+            raw: true,
+        }),
+    ]
 };
