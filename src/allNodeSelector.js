ddfdc8f28c7d8a975d6f85eb06c563f1b68b5ca1
fosemberg <fosemberg@gmail.com>
2020-02-01 09:06:59 +0300

    all node selector

---
+++ src/allNodeSelector.js
@@ -0,0 +1,15 @@
+(() => {
+    const TEXT_NODE = 3;
+    document.querySelectorAll('blockquote,summary,b,th,code,ul,li,button,pre,em,div,p,td,a,h1,h2,h3,h4,h5,h6,cite,nobr,span').forEach(el => {
+        if (el.childElementCount === 0) {
+            const {innerText} = el;
+            if (innerText) el.style.color = 'red';
+        } else {
+            el.childNodes.forEach(child => {
+                if (child.nodeType === TEXT_NODE) {
+                    child.textContent = `[${child.textContent}]`;
+                }
+            })
+        }
+    })
+})();
